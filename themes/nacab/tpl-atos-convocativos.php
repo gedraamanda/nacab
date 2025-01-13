54babba584bc3e1602ffc73b9c26e2de5b35ab4b
macbook Pro <macbookpro@MacBook-Pro-de-macbook.local>
2025-01-13 17:38:44 -0300

    atos e editais

---
+++ themes/nacab/tpl-atos-convocativos.php
@@ -0,0 +1,130 @@
+<?php
+
+
+/* Template Name: Atos convocativos */
+
+get_header();
+?>
+	<section class="atos_section pt-2">
+		<div class="container">
+			<h2>oportunidades</h2>
+			<div class="row">
+				<div class="col-sm-2"></div>
+				<div class="col-sm-8">
+					<div class="menu_atos">
+						<ul>
+							<li class="active"><a href="#"><b>Atos convocatórios</b></a></li>
+							<li><a href="#">Editais</a></li>
+						</ul>
+					</div>
+				</div>
+				<div class="col-sm-2"></div>
+			</div>
+		</div>
+	</section>
+	<section id="atos_contant">
+		<div class="container">
+			<section class="ati_btns pt-4 pb-4">
+				<div class="row pt-3">
+					<div class="col-sm-2"></div>
+					<div class="col-sm-8">
+						<ul>
+							<li><a href="#"><button>ATI Paraopeba Nacab</button></a></li>
+							<li><a href="#"><button>ATI 39</button></a></li>
+						</ul>
+					</div>
+					<div class="col-sm-2"></div>
+				</div>
+			</section>
+			<section>
+				<div class="row">
+					<div class="col-sm-2"></div>
+					<div class="col-sm-8">
+						<div class="atos_accorion mb-5">
+							<div class="accordion" id="accordionExample">
+								<div class="accordion-item">
+									<h2 class="accordion-header" id="headingOne">
+										<button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="false" aria-controls="collapseOne">ATO CONVOCATÓRIO: 007/2021
+										</button>
+									</h2>
+									<div id="collapseOne" class="accordion-collapse collapse show" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
+										<div class="accordion-body">
+											<div class="convocatorio_section">
+												<p>O NÚCLEO DE ASSESSORIA ÀS COMUNIDADES ATINGIDAS POR BARRAGENS – NACAB, no uso de suas atribuições legais e regulamentares, torna pública a realização de Processo Seletivo para Contratação de Pessoal e para Formação de Cadastro Reserva no âmbito do projeto de Assessoria Técnica Independente de reparação integral de danos às pessoas atingidas pelo rompimento do complexo de barragens da mina do Córrego do Feijão em Brumadinho/MG da Vale S.A., Bacia do Paraopeba – Região 3.</p>
+												<ul class="mt-2 mb-0">
+													<li>Abertura: <span>23/04/2024 </span></li>
+													<li>Inscrições: <span>Até 23h59min do dia 28/04/2024</span></li>
+													<li class="my-1 pb-1"><a href="#">Se inscreva aqui</a></li>
+												</ul>
+											</div>
+											<div class="download_section">
+												<h5 class="mb-0">Downloads:</h5>
+												<ul>
+													<li><a href="#">Edital</a></li>
+													<li><a href="#">Anexo 1 – <span>Autodeclaração de pessoa com deficiência</span></a></li>
+													<li><a href="#">Anexo 2 – <span>Autodeclaração étnico-racial</span></a></li>
+													<li><a href="#">Prorrogação Prazo de Inscrição</a></li>
+													<li><a href="#">Lista de Candidatos/as Selecionados/as para Entrevista <span>(13/05/2024 17:28)</span></a></li>
+													<li><a href="#">Lista Final Selecionados/as para entrevista – Datas e Horários <span>(15/05/2024 17:25)</span></a></li>
+													<li><a href="#">Resultado Entrevistas <span>(27/05/2024 – 18:37 – atual. 19:07)</span></a></li>
+													<li><a href="#">Resultado Final <span>(29/05/2024 – 16:27)</span></a></li>
+												</ul>
+											</div>
+										</div>
+									</div>
+								</div>
+								<div class="accordion-item">
+									<h2 class="accordion-header" id="headingTwo">
+										<button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">ATO CONVOCATÓRIO: 006/2021
+										</button>
+									</h2>
+									<div id="collapseTwo" class="accordion-collapse collapse" aria-labelledby="headingTwo" data-bs-parent="#accordionExample">
+										<div class="accordion-body mb-3">Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+										</div>
+									</div>
+								</div>
+								<div class="accordion-item">
+									<h2 class="accordion-header" id="headingThree">
+										<button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseTwo">ATO CONVOCATÓRIO: 005/2021
+										</button>
+									</h2>
+									<div id="collapseThree" class="accordion-collapse collapse" aria-labelledby="headingThree" data-bs-parent="#accordionExample">
+										<div class="accordion-body mb-3">Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+										</div>
+									</div>
+								</div>
+								<div class="accordion-item">
+									<h2 class="accordion-header" id="headingFour">
+										<button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseFour" aria-expanded="false" aria-controls="collapseTwo">ATO CONVOCATÓRIO: 004/2021
+										</button>
+									</h2>
+									<div id="collapseFour" class="accordion-collapse collapse" aria-labelledby="headingFour" data-bs-parent="#accordionExample">
+										<div class="accordion-body mb-3">Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+										</div>
+									</div>
+								</div>
+								<div class="accordion-item">
+									<h2 class="accordion-header" id="headingFive">
+										<button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseFive" aria-expanded="false" aria-controls="collapseTwo">ATO CONVOCATÓRIO: 003/2021 - Republicação
+										</button>
+									</h2>
+									<div id="collapseFive" class="accordion-collapse collapse" aria-labelledby="headingFive" data-bs-parent="#accordionExample">
+										<div class="accordion-body mb-3">Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+										</div>
+									</div>
+								</div>
+
+							</div>
+						</div>
+
+
+
+					</div>
+					<div class="col-sm-2"></div>
+				</div>
+			</section>
+		</div>
+
+	</section>
+<?php
+get_footer();
---
+++ themes/nacab/tpl-editais.php
@@ -0,0 +1,116 @@
+<?php
+
+
+/* Template Name: Editais */
+
+get_header();
+?>
+	<section class="atos_section pt-2">
+		<div class="container">
+			<h2>oportunidades</h2>
+			<div class="row">
+				<div class="col-sm-2"></div>
+				<div class="col-sm-8">
+					<div class="menu_atos">
+						<ul>
+							<li class=""><a href="#">Atos convocatórios</a></li>
+							<li class="active"><a href="#"><b>Editais</b></a></li>
+						</ul>
+					</div>
+				</div>
+				<div class="col-sm-2"></div>
+			</div>
+		</div>
+	</section>
+	<section id="atos_contant">
+		<div class="container">
+			<section class="editais_btns pt-4">
+				<div class="row pt-3">
+					<div class="col-sm-2"></div>
+					<div class="col-sm-8">
+						<ul>
+							<li><a href="#"><button>ATI Paraopeba Nacab</button></a></li>
+							<li><a href="#"><button>ATI 39</button></a></li>
+						</ul>
+					</div>
+					<div class="col-sm-2"></div>
+				</div>
+			</section>
+			<section class="editais_btns_b pb-4">
+				<div class="row pt-2">
+					<div class="col-sm-2"></div>
+					<div class="col-sm-8">
+						<ul>
+							<li><a href="#"><button>Editais ativos</button></a></li>
+							<li><a href="#"><button>Editais passados</button></a></li>
+						</ul>
+					</div>
+					<div class="col-sm-2"></div>
+				</div>
+			</section>
+			<section>
+				<div class="row">
+					<div class="col-sm-2"></div>
+					<div class="col-sm-8">
+						<div class="editais_contant">
+							<div class="convocatorio_section">
+								<h1 class="pt-2">2024</h1>
+								<h3>PROCESSO SELETIVO SIMPLIFICADO – EDITAL 01/2024</h3>
+								<p>O NÚCLEO DE ASSESSORIA ÀS COMUNIDADES ATINGIDAS POR BARRAGENS – NACAB, no uso de suas atribuições legais e regulamentares, torna pública a realização de Processo Seletivo para Contratação de Pessoal e para Formação de Cadastro Reserva no âmbito do projeto de Assessoria Técnica Independente de reparação integral de danos às pessoas atingidas pelo rompimento do complexo de barragens da mina do Córrego do Feijão em Brumadinho/MG da Vale S.A., Bacia do Paraopeba – Região 3.</p>
+								<ul class="mt-2 mb-0">
+									<li>Abertura: <span>23/04/2024 </span></li>
+									<li>Inscrições: <span>Até 23h59min do dia 28/04/2024</span></li>
+									<li class="my-1 pb-1"><a href="#">Se inscreva aqui</a></li>
+								</ul>
+							</div>
+							<div class="download_section">
+								<h5 class="mb-0">Downloads:</h5>
+								<ul>
+									<li><a href="#">Edital</a></li>
+									<li><a href="#">Anexo 1 – <span>Autodeclaração de pessoa com deficiência</span></a></li>
+									<li><a href="#">Anexo 2 – <span>Autodeclaração étnico-racial</span></a></li>
+									<li><a href="#">Prorrogação Prazo de Inscrição</a></li>
+									<li><a href="#">Lista de Candidatos/as Selecionados/as para Entrevista <span>(13/05/2024 17:28)</span></a></li>
+									<li><a href="#">Lista Final Selecionados/as para entrevista – Datas e Horários <span>(15/05/2024 17:25)</span></a></li>
+									<li><a href="#">Resultado Entrevistas <span>(27/05/2024 – 18:37 – atual. 19:07)</span></a></li>
+									<li><a href="#">Resultado Final <span>(29/05/2024 – 16:27)</span></a></li>
+								</ul>
+							</div>
+						</div>
+						<div class="editais_contant mt-4">
+							<div class="convocatorio_section">
+								<h1 class="pt-2">2023</h1>
+								<h3>PROCESSO SELETIVO SIMPLIFICADO – EDITAL 02/2023</h3>
+								<p>O NÚCLEO DE ASSESSORIA ÀS COMUNIDADES ATINGIDAS POR BARRAGENS – NACAB, no uso de suas atribuições legais e regulamentares, torna pública a realização de Processo Seletivo para Contratação de Pessoal e para Formação de Cadastro Reserva no âmbito do projeto de Assessoria Técnica Independente de reparação integral de danos às pessoas atingidas pelo rompimento do complexo de barragens da mina do Córrego do Feijão em Brumadinho/MG da Vale S.A., Bacia do Paraopeba – Região 3.</p>
+								<ul class="mt-2 mb-0">
+									<li>Abertura: <span>23/04/2024 </span></li>
+									<li>Inscrições: <span>Até 23h59min do dia 28/04/2024</span></li>
+									<li class="my-1 pb-1"><a href="#">Se inscreva aqui</a></li>
+								</ul>
+							</div>
+							<div class="download_section">
+								<h5 class="mb-0">Downloads:</h5>
+								<ul>
+									<li><a href="#">Edital</a></li>
+									<li><a href="#">Anexo 1 – <span>Autodeclaração de pessoa com deficiência</span></a></li>
+									<li><a href="#">Anexo 2 – <span>Autodeclaração étnico-racial</span></a></li>
+									<li><a href="#">Prorrogação Prazo de Inscrição</a></li>
+									<li><a href="#">Lista de Candidatos/as Selecionados/as para Entrevista <span>(13/05/2024 17:28)</span></a></li>
+									<li><a href="#">Lista Final Selecionados/as para entrevista – Datas e Horários <span>(15/05/2024 17:25)</span></a></li>
+									<li><a href="#">Resultado Entrevistas <span>(27/05/2024 – 18:37 – atual. 19:07)</span></a></li>
+									<li><a href="#">Resultado Final <span>(29/05/2024 – 16:27)</span></a></li>
+								</ul>
+							</div>
+						</div>
+						<div class="editais_pass my-5">
+							<a href="#">Editais passados</a>
+						</div>
+					</div>
+					<div class="col-sm-2"></div>
+				</div>
+			</section>
+		</div>
+
+	</section>
+<?php
+get_footer();
