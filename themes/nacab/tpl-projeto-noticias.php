1f51e7034f76a2e122412ca2b49e8d3af5ee3901
macbook Pro <macbookpro@MacBook-Pro-de-macbook.local>
2025-01-15 11:19:01 -0300

    Projeto noticias

---
+++ themes/nacab/tpl-projeto-noticias.php
@@ -0,0 +1,159 @@
+<?php
+
+
+/* Template Name: projeto - noticias */
+
+get_header();
+?>
+	<section class="projetos_section pt-2">
+		<div class="container">
+			<h2>projetos</h2>
+			<div class="row">
+
+				<div class="col-sm-10 mx-auto">
+					<div class="menu_projectos">
+						<ul class="principal mx-auto" style="--bdc: #84AF9C">
+							<li class="active"><a href="#"><b>Paraopeba</b></a></li>
+							<li><a href="#">Mina do Sapo</a></li>
+							<li><a href="#">Cachoeira Grande</a></li>
+						</ul>
+
+						<div class="sub">
+							<ul class="sub_menu mx-5">
+								<li class=""><a href="#">Notícias</a></li>
+								<li><a href="#">Sobre</a></li>
+								<li><a href="#">Linha do tempo</a></li>
+								<li><a href="#">Estudos e publicações</a></li>
+								<li><a href="#">Documentos importantes</a></li>
+							</ul>
+						</div>
+					</div>
+				</div>
+
+			</div>
+		</div>
+	</section>
+
+    <section class="py-5 section-color ultimas-noticias-destaque" style="--bgc: #D0E7DD">
+        <div class="container">
+            <div class="row post pt-4 pe-5 mb-5" id="all">
+                <div class="col-sm-6">
+                    <a href="#">
+                        <img src="<?php echo get_template_directory_uri() ?>/assets/img/post-c.png">
+                    </a>
+                </div>
+                <div class="col-sm-6 pe-5 pt-5">
+                    <a href="#"><h3>Representantes de Povos e Comunidades Tradicionais da Região 3 aprendem sobre o uso do bambu</h3></a>
+                    <p class="m-0 mt-3">De cada quatro decisões, três foram desfavoráveis aos atingidos pela rompimento da barragem da mineradora Vale</p>
+
+                    <p class="cat-date mt-3">/  5.11.2024</p>
+                </div>
+            </div>
+
+
+            <div class="row post pt-4" id="all">
+                <div class="col-sm-6 ps-5">
+                    <a href="#">
+                        <img src="<?php echo get_template_directory_uri() ?>/assets/img/post-e.png">
+                    </a>
+
+                    <a href="#"><h3>Representantes de Povos e Comunidades Tradicionais da Região 3 aprendem sobre o uso do bambu</h3></a>
+                    <p class="m-0 mt-3">De cada quatro decisões, três foram desfavoráveis aos atingidos pela rompimento da barragem da mineradora Vale</p>
+
+                    <p class="cat-date mt-3">/  5.11.2024</p>
+                </div>
+
+                <div class="col-sm-6 ps-5 top_margin">
+                    <a href="#">
+                        <img src="<?php echo get_template_directory_uri() ?>/assets/img/post-d.png">
+                    </a>
+
+                    <a href="#"><h3>Representantes de Povos e Comunidades Tradicionais da Região 3 aprendem sobre o uso do bambu</h3></a>
+                    <p class="m-0 mt-3">De cada quatro decisões, três foram desfavoráveis aos atingidos pela rompimento da barragem da mineradora Vale</p>
+
+                    <p class="cat-date mt-3">/  5.11.2024</p>
+                </div>
+            </div>
+        </div>
+
+
+    </section>
+
+    <section class="video_post my-4 py-5">
+        <div class="container">
+            <div class="mx-5 px-5">
+                <div class="row">
+                    <div class="col-sm-4">
+                        <img src="<?php echo get_template_directory_uri() ?>/assets/img/MINA_a.jpg">
+                        <h4 class="pe-4"><a href="#">Comunidades da ZAS enviam carta à Anglo American solicitando mais prazo para as negociações</a></h4>
+                        <p class="pt-2">/  5.11.2024</p>
+                    </div>
+                    <div class="col-sm-4">
+                        <img src="<?php echo get_template_directory_uri() ?>/assets/img/MINA_b.jpg">
+                        <h4 class="pe-4"><a href="#">Córregos celebra com canto e dança a fé em Nossa Senhora do Rosário</a></h4>
+                        <p class="pt-2">/  5.11.2024</p>
+                    </div>
+                    <div class="col-sm-4">
+                        <img src="<?php echo get_template_directory_uri() ?>/assets/img/MINA_c.jpg">
+                        <h4 class="pe-4"><a href="#">Celebração à Nossa Senhora da Aparecida dos Córregos</a></h4>
+                        <p class="pt-2">/  5.11.2024</p>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-sm-4">
+                        <img src="<?php echo get_template_directory_uri() ?>/assets/img/dummy.jpg">
+                        <h4 class="pe-4"><a href="#">Córregos celebra com canto e dança a fé em Nossa Senhora do Rosário </a></h4>
+                        <p class="pt-2">/  5.11.2024</p>
+                    </div>
+                    <div class="col-sm-4">
+                        <img src="<?php echo get_template_directory_uri() ?>/assets/img/dummy.jpg">
+                        <h4 class="pe-4"><a href="#">Comunidades da ZAS enviam carta à Anglo American solicitando mais prazo para as negociações</a></h4>
+                        <p class="pt-2">/  5.11.2024</p>
+                    </div>
+                    <div class="col-sm-4">
+                        <img src="<?php echo get_template_directory_uri() ?>/assets/img/dummy.jpg">
+                        <h4 class="pe-4"><a href="#">Celebração à Nossa Senhora da Aparecida dos Córregos</a></h4>
+                        <p class="pt-2">/  5.11.2024</p>
+                    </div>
+                </div>
+
+                <section id="youtube_section" class="mb-5">
+                    <div class="container px-5 pb-5 ">
+                        <div class="y_video_section">
+                            <div class="row">
+                                <div class="col-sm-5">
+                                    <img src="<?php echo get_template_directory_uri() ?>/assets/img/vozes-video.png">
+                                </div>
+                                <div class="col-sm-7">
+                                    <h2>VOZES ATINGIDAS</h2>
+                                    <h5 class="pe-5 pb-2">Relatos do Paraopeba. No primeiro episódio, o pescador Pedro conta da sua relação com o Paraopeba e o que a perda do rio significa para ele.</h5>
+                                    <a href="#">assista agora</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </section>
+                
+                <div class="row">
+                    <div class="col-sm-4">
+                        <img src="<?php echo get_template_directory_uri() ?>/assets/img/dummy.jpg">
+                        <h4 class="pe-4"><a href="#">Córregos celebra com canto e dança a fé em Nossa Senhora do Rosário </a></h4>
+                        <p class="pt-2">/  5.11.2024</p>
+                    </div>
+                    <div class="col-sm-4">
+                        <img src="<?php echo get_template_directory_uri() ?>/assets/img/dummy.jpg">
+                        <h4 class="pe-4"><a href="#">Comunidades da ZAS enviam carta à Anglo American solicitando mais prazo para as negociações</a></h4>
+                        <p class="pt-2">/  5.11.2024</p>
+                    </div>
+                    <div class="col-sm-4">
+                        <img src="<?php echo get_template_directory_uri() ?>/assets/img/dummy.jpg">
+                        <h4 class="pe-4"><a href="#">Celebração à Nossa Senhora da Aparecida dos Córregos</a></h4>
+                        <p class="pt-2">/  5.11.2024</p>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    </section>
+<?php
+get_footer();
