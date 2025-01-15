66d2d46cb3f86ddfb29f2177a7049a96a43b3289
macbook Pro <macbookpro@MacBook-Pro-de-macbook.local>
2025-01-15 11:18:52 -0300

    Projeto noticias

--- themes/nacab/assets/js/scripts.js
+++ themes/nacab/assets/js/scripts.js
@@ -18,9 +18,6 @@ function openAtos() {
     document.getElementById("myDropdown").classList.toggle("show");
 }
 
-function openMenuInt() {
-    document.getElementById("myDropdown").classList.toggle("show");
-}
 
 // Close the dropdown if the user clicks outside of it
 // window.onclick = function(event) {
