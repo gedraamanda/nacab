9d1adedd7663ce984c3c5fae322038534ac2d857
macbook Pro <macbookpro@MacBook-Pro-de-macbook.local>
2025-01-17 10:01:44 -0300

    estudos

---
+++ themes/nacab/tpl-projeto-estudos.php
@@ -0,0 +1,323 @@
+<?php
+
+
+/* Template Name: Projeto - estudos */
+
+get_header();
+?>
+<section class="projetos_section pt-2">
+	<div class="container">
+		<h2>projetos</h2>
+		<div class="row">
+
+			<div class="col-sm-10 mx-auto">
+				<div class="menu_projectos">
+					<ul class="principal mx-auto" style="--bdc: #84AF9C">
+						<li class="active"><a href="#"><b>Paraopeba</b></a></li>
+						<li><a href="#">Mina do Sapo</a></li>
+						<li><a href="#">Cachoeira Grande</a></li>
+					</ul>
+
+					<div class="sub">
+						<ul class="sub_menu mx-5">
+							<li class=""><a href="#">Notícias</a></li>
+							<li><a href="#" >Sobre</a></li>
+							<li><a href="#" >Linha do tempo</a></li>
+							<li><a href="#" class="ativo">Estudos e publicações</a></li>
+							<li><a href="#">Documentos importantes</a></li>
+						</ul>
+					</div>
+				</div>
+			</div>
+
+		</div>
+	</div>
+</section>
+
+	<section id="estudos_contant">
+		<section class="information_section">
+			<div class="container">
+				<div class="row pt-2">
+					<div class="col-sm-2"></div>
+					<div class="col-sm-8 pb-4">
+						<div class="estudos_title py-4">
+							<a href="#">Estudos de Avaliação de Risco à Saúde Humana e Risco Ecológico</a>
+						</div>
+
+						<div class="information_accorion mb-5">
+							<div class="accordion mt-4" id="accordionExample">
+								<div class="accordion-item">
+									<h2 class="accordion-header" id="headingOne">
+										<button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="false" aria-controls="collapseOne">O que são os Estudos de avaliação de risco à saúde humana e risco ecológico (ERSHRE)?
+										</button>
+									</h2>
+									<div id="collapseOne" class="accordion-collapse collapse show" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
+										<div class="accordion-body">O rompimento da barragem BI, da mina do Córrego do Feijão, da empresa Vale S.A, em Brumadinho (2019), lançou uma grande quantidade de rejeito de minério no ambiente, atingindo o rio Paraopeba. Frente a essa realidade, Estudos de Avaliação de Risco à Saúde Humana e Risco Ecológico (ERSHRE) foram demandados por órgãos de saúde e meio ambiente do Estado de Minas Gerais, em parceria com as Instituições de Justiça, visto os potenciais riscos e impactos que a exposição ao rejeito de minério pode causar à saúde das pessoas, bem como ao meio ambiente.
+										</div>
+									</div>
+								</div>
+								<div class="accordion-item">
+									<h2 class="accordion-header" id="headingTwo">
+										<button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">Quem realiza esses estudos?
+										</button>
+									</h2>
+									<div id="collapseTwo" class="accordion-collapse collapse" aria-labelledby="headingTwo" data-bs-parent="#accordionExample">
+										<div class="accordion-body">Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+										</div>
+									</div>
+								</div>
+								<div class="accordion-item">
+									<h2 class="accordion-header" id="headingThree">
+										<button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseTwo">Quais são as fases desses estudos e em que etapa eles se encontram?
+										</button>
+									</h2>
+									<div id="collapseThree" class="accordion-collapse collapse" aria-labelledby="headingThree" data-bs-parent="#accordionExample">
+										<div class="accordion-body">Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+										</div>
+									</div>
+								</div>
+								<div class="accordion-item">
+									<h2 class="accordion-header" id="headingFour">
+										<button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseFour" aria-expanded="false" aria-controls="collapseTwo">Como as pessoas atingidas participam?
+										</button>
+									</h2>
+									<div id="collapseFour" class="accordion-collapse collapse" aria-labelledby="headingFour" data-bs-parent="#accordionExample">
+										<div class="accordion-body">Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+										</div>
+									</div>
+								</div>
+								<div class="accordion-item">
+									<h2 class="accordion-header" id="headingFive">
+										<button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseFive" aria-expanded="false" aria-controls="collapseTwo">O que pode ser esperado desses estudos?
+										</button>
+									</h2>
+									<div id="collapseFive" class="accordion-collapse collapse" aria-labelledby="headingFive" data-bs-parent="#accordionExample">
+										<div class="accordion-body">Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+										</div>
+									</div>
+								</div>
+								<div class="accordion-item">
+									<h2 class="accordion-header" id="headingSix">
+										<button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseSix" aria-expanded="false" aria-controls="collapseTwo">Qual a atuação das Assessorias Técnicas Independentes (ATIs) nos ERSHRE?
+										</button>
+									</h2>
+									<div id="collapseSix" class="accordion-collapse collapse" aria-labelledby="headingSix" data-bs-parent="#accordionExample">
+										<div class="accordion-body">Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+										</div>
+									</div>
+								</div>
+								<div class="accordion-item">
+									<h2 class="accordion-header" id="headingSevan">
+										<button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseSevan" aria-expanded="false" aria-controls="collapseTwo">Como se dá a participação de Povos e Comunidades Tradicionais?
+										</button>
+									</h2>
+									<div id="collapseSevan" class="accordion-collapse collapse" aria-labelledby="headingSevan" data-bs-parent="#accordionExample">
+										<div class="accordion-body">Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+										</div>
+									</div>
+								</div>
+
+							</div>
+						</div>
+
+					</div>
+					<div class="col-sm-2"></div>
+				</div>
+			</div>
+		</section>
+	</section>
+
+	<section id="Germinar_page">
+		<div class="container">
+			<div class="row Germinar_section">
+				<div class="col-sm-1"></div>
+				<div class="col-sm-10 mt-5 big_owl">
+					<h3 class="pt-2">Germinar</h3>
+					<p class="pt-1 b_bottom">A publicação Germinar é produzida por ... periodicamente e tem como tema</p>
+					<div class="row">
+						<div class="col-sm-2"></div>
+						<div class="col-sm-8">
+							<div class="owl-carousel owl_one owl-theme">
+								<div class="item">
+									<div class="section_a">
+										<div class="contant_section">
+											<img src="<?php echo get_template_directory_uri() ?>/assets/img/Germinar.png">
+										</div>
+									</div>
+								</div>
+								<div class="item">
+									<div class="section_a">
+										<div class="contant_section">
+											<img src="<?php echo get_template_directory_uri() ?>/assets/img/Germinar.png">
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+						<div class="col-sm-2"></div>
+					</div>
+
+				</div>
+				<div class="col-sm-1"></div>
+			</div>
+			<div class="row Germinar_section">
+				<div class="col-sm-1"></div>
+				<div class="col-sm-10 mt-5">
+					<h3 class="pt-2">relatórios de atividades</h3>
+					<p class="pt-1 b_bottom">A publicação Germinar é produzida por ... periodicamente e tem como tema blahblah</p>
+					<div class="owl-carousel owl_two owl-theme pb-3">
+						<div class="item">
+							<div class="section_a">
+								<div class="contant_section">
+									<img src="<?php echo get_template_directory_uri() ?>/assets/img/relatorios_a.png">
+								</div>
+							</div>
+						</div>
+						<div class="item">
+							<div class="section_a">
+								<div class="contant_section">
+									<img src="<?php echo get_template_directory_uri() ?>/assets/img/relatorios_b.png">
+								</div>
+							</div>
+						</div>
+						<div class="item">
+							<div class="section_a">
+								<div class="contant_section">
+									<img src="<?php echo get_template_directory_uri() ?>/assets/img/relatorios_c.png">
+								</div>
+							</div>
+						</div>
+						<div class="item">
+							<div class="section_a">
+								<div class="contant_section">
+									<img src="<?php echo get_template_directory_uri() ?>/assets/img/relatorios_d.png">
+								</div>
+							</div>
+						</div>
+						<div class="item">
+							<div class="section_a">
+								<div class="contant_section">
+									<img src="<?php echo get_template_directory_uri() ?>/assets/img/relatorios_e.png">
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="col-sm-1"></div>
+			</div>
+			<div class="row Germinar_section">
+				<div class="col-sm-1"></div>
+				<div class="col-sm-10 mt-5">
+					<h3 class="pt-2">mobilização</h3>
+					<p class="pt-1 b_bottom">A publicação Germinar é produzida por ... periodicamente e tem como tema blahblah</p>
+					<div class="owl-carousel owl_two owl-theme pb-3">
+						<div class="item">
+							<div class="section_a">
+								<div class="contant_section">
+									<img src="<?php echo get_template_directory_uri() ?>/assets/img/mobilizacao_a.png">
+								</div>
+							</div>
+						</div>
+						<div class="item">
+							<div class="section_a">
+								<div class="contant_section">
+									<img src="<?php echo get_template_directory_uri() ?>/assets/img/mobilizacao_b.png">
+								</div>
+							</div>
+						</div>
+						<div class="item">
+							<div class="section_a">
+								<div class="contant_section">
+									<img src="<?php echo get_template_directory_uri() ?>/assets/img/mobilizacao_c.png">
+								</div>
+							</div>
+						</div>
+						<div class="item">
+							<div class="section_a">
+								<div class="contant_section">
+									<img src="<?php echo get_template_directory_uri() ?>/assets/img/mobilizacao_d.png">
+								</div>
+							</div>
+						</div>
+						<div class="item">
+							<div class="section_a">
+								<div class="contant_section">
+									<img src="<?php echo get_template_directory_uri() ?>/assets/img/mobilizacao_e.png">
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="col-sm-1"></div>
+			</div>
+			<div class="row Germinar_section">
+				<div class="col-sm-1"></div>
+				<div class="col-sm-10 mt-5">
+					<h3 class="pt-2">jornal reparação</h3>
+					<p class="pt-1 b_bottom">A publicação Germinar é produzida por ... periodicamente e tem como tema blahblah</p>
+					<div class="row">
+						<div class="col-sm-3"></div>
+						<div class="col-sm-6 big_owl">
+							<div class="owl-carousel owl_three owl-theme pb-3">
+								<div class="item">
+									<div class="section_a">
+										<div class="contant_section">
+											<img src="<?php echo get_template_directory_uri() ?>/assets/img/jornal_a.png">
+										</div>
+									</div>
+								</div>
+								<div class="item">
+									<div class="section_a">
+										<div class="contant_section">
+											<img src="<?php echo get_template_directory_uri() ?>/assets/img/jornal_b.png">
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+						<div class="col-sm-3"></div>
+					</div>
+				</div>
+				<div class="col-sm-1"></div>
+			</div>
+			<div class="row Germinar_section pb-5">
+				<div class="col-sm-1"></div>
+				<div class="col-sm-10 mt-5">
+					<h3 class="pt-2">Estudo Jurisprudencial </h3>
+					<p class="pt-1 b_bottom">Comportamento do Tribunal de Justiça de MG perante as ações individuais ajuizadas por pessoas atingidas</p>
+					<div class="row">
+						<div class="col-sm-4"></div>
+						<div class="col-sm-4 big_owl">
+							<div class="owl-carousel owl_one owl-theme">
+								<div class="item">
+									<div class="section_a">
+										<div class="contant_section">
+											<img src="<?php echo get_template_directory_uri() ?>/assets/img/Estudo.png">
+										</div>
+									</div>
+								</div>
+								<div class="item">
+									<div class="section_a">
+										<div class="contant_section">
+											<img src="<?php echo get_template_directory_uri() ?>/assets/img/Estudo.png">
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+						<div class="col-sm-4"></div>
+					</div>
+
+				</div>
+				<div class="col-sm-1"></div>
+			</div>
+		</div>
+	</section>
+
+	<section class="my-5">
+		<div class="py-5"></div>
+	</section>
+
+
+<?php
+get_footer();
